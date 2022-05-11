37597ba983eab7d6d231c185ad2eeeb7c6e25e87
JungYiChim <ffddfdsfa88@gmail.com>
2022-05-11 21:25:01 +0900

    마지막수정
    ㅋㅋㅋ

--- src/main/java/com/example/dao/DogDAO.java
+++ src/main/java/com/example/dao/DogDAO.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.example.domain.DogKindVO;
 
+//마이바티즈연결 DAO 인터페이스
 @Mapper
 public interface DogDAO {
 	public DogKindVO getDog(DogKindVO vo);
