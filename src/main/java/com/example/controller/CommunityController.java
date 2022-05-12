8382c18b59bd0d7f2d965249958d8c5981958bb3
zzeennee <96711823+zzeennee@users.noreply.github.com>
2022-05-12 10:21:14 +0900

    인덱스 페이지 수정
    인덱스 페이지 수정

---
+++ src/main/java/com/example/controller/CommunityController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class CommunityController {
+
+
+@Controller
+@RequestMapping("/include/community")
+public class HomeController {
+
+	@RequestMapping("/daily")
+	public void HeaderPage() {
+	}
+	
+	}
+	
+}
